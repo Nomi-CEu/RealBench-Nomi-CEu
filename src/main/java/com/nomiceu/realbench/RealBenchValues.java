28445404f7f1e9f20e942b9da6e6351d366fc5ec
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-12-02 17:32:36 +1100

    RealBench: Nomi CEu Edition -> RealBench: Nomi-CEu Edition

--- src/main/java/com/nomiceu/realbench/RealBenchValues.java
+++ src/main/java/com/nomiceu/realbench/RealBenchValues.java
@@ -2,5 +2,5 @@ package com.nomiceu.realbench;
 
 public class RealBenchValues {
     public static final String MODID = "realbench";
-    public static final String MODNAME = "RealBench: Nomi CEu Edition";
+    public static final String MODNAME = "RealBench: Nomi-CEu Edition";
 }
